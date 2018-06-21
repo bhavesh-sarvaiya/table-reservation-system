6b04d312b4f40d25e12eb96ad32032d0df9daa57
Bhavesh Sarvaiya <bhaveshsarvaiya577@gmail.com>
2018-06-21 20:43:07 +0530

    Reverse commit for 'Add Hotel entity'.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,13 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { TableReservationHotelModule } from './hotel/hotel.module';
 import { TableReservationHotelTableModule } from './hotel-table/hotel-table.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
-        TableReservationHotelModule,
+
         TableReservationHotelTableModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
--- src/main/webapp/app/entities/hotel/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './hotel.service';
-export * from './hotel-update.component';
-export * from './hotel-delete-dialog.component';
-export * from './hotel-detail.component';
-export * from './hotel.component';
-export * from './hotel.route';
